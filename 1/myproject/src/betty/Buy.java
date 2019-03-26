ae30a3b03925542820d2575a4cfce3923509401d
user <user@es301n16>
2019-03-26 12:02:07 +0800

    1

---
+++ 1/myproject/src/betty/Buy.java
@@ -0,0 +1,40 @@
+package betty;
+
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Buy {
+
+	public static void main(String[] args) {
+		List<Integer> list = new ArrayList<>();
+		Scanner  scanner = new Scanner(System.in);
+		String name = "";
+		while(!name.equals("q"))
+			System.out.println("please enter item name:");
+			String  name= scanner.nextLine();
+		    System.out.println("please enter quantity:");
+		    int qty = scanner.nextInt();
+		    item.add(new Item(name, qty) );
+		    for(Item item;items) {
+			System.out.println(item.name+"/t"+item.qty);
+		}
+		
+		
+		
+        }
+		
+	}
+
+			
+		
+		
+		
+		
+		
+				
+		
+	
+
+
---
+++ 1/myproject/src/betty/Item.java
@@ -0,0 +1,10 @@
+package betty;
+
+public class Item {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
